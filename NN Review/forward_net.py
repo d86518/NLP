a1df70c282ec7fd4c15c4bfd564c6a5d399d6607
David Huang <d86518.ms04@g2.nctu.edu.tw>
2019-05-22 08:25:16 +0800

    Deep Learning
    Deep Learning

---
+++ NN Review/forward_net.py
@@ -0,0 +1,63 @@
+﻿# coding: utf-8
+"""
+練習實作forward_net
+此結構為 Input -> Affine層 -> Sigmoid層 -> Affine層 -> Output
+1. 在各層執行forward
+2. 參數清單存params
+3. 梯度清單存grads(後面用)
+
+"""
+import numpy as np
+
+
+class Sigmoid:
+    def __init__(self):
+        self.params = []
+
+    def forward(self, x):
+        return 1 / (1 + np.exp(-x))
+
+
+class Affine:
+    def __init__(self, W, b):
+        self.params = [W, b]
+
+    def forward(self, x):
+        W, b = self.params
+        out = np.dot(x, W) + b
+        return out
+
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size):
+        I, H, O = input_size, hidden_size, output_size
+
+        # initialize weight and bias
+        W1 = np.random.randn(I, H)
+        b1 = np.random.randn(H)
+        W2 = np.random.randn(H, O)
+        b2 = np.random.randn(O)
+
+        # 產生各層
+        self.layers = [
+            Affine(W1, b1),
+            Sigmoid(),
+            Affine(W2, b2)
+        ]
+
+        # 把所有權重整合成清單
+        self.params = []
+        for layer in self.layers:
+            self.params += layer.params
+
+    def predict(self, x):
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+
+
+x = np.random.randn(10, 2)
+model = TwoLayerNet(2, 4, 3)
+s = model.predict(x)
+print("Input:",x)
+print("Output:",s)
---
+++ rnnlm_train.py
@@ -0,0 +1,32 @@
+﻿# coding: utf-8
+import sys
+sys.path.append('..')
+from common.optimizer import SGD
+from common.trainer import RnnlmTrainer
+from dataset import ptb
+from simple_rnnlm import SimpleRnnlm
+
+
+# 設定超參數
+batch_size = 10
+wordvec_size = 100
+hidden_size = 100  # RNN的隱藏狀態向量的元素數
+time_size = 5  # 展開RNN的大小
+lr = 0.1
+max_epoch = 100
+
+# 載入學習資料
+corpus, word_to_id, id_to_word = ptb.load_data('train')
+corpus_size = 1000  # 縮小測試用的資料集corpus = corpus[:corpus_size]
+vocab_size = int(max(corpus) + 1)
+xs = corpus[:-1]  # 輸入
+ts = corpus[1:]  # 輸出（訓練標籤）
+
+# 產生模型
+model = SimpleRnnlm(vocab_size, wordvec_size, hidden_size)
+optimizer = SGD(lr)
+trainer = RnnlmTrainer(model, optimizer)
+
+trainer.fit(xs, ts, max_epoch, batch_size, time_size)
+trainer.plot()
+
